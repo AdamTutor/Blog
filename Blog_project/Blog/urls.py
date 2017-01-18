b85160e7ed342dcd7bef2bf39bc327e1d8374c52
Adam Tutor <atutor@basecampcodingacademy.org>
2017-01-18 10:18:41 -0600

    added template folder and new urls/views to test template rendering

--- Blog_project/Blog/urls.py
+++ Blog_project/Blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^(?P<id>[0-9]+)$', views.detail, name="details"),
     url(r'^create', views.create, name="create"),
     url(r'^delete', views.delete, name="delete"),
     url(r'^edit', views.edit, name="edit"),
+    url(r'^test', views.test, name="test"),
+
 ]
--- Blog_project/Blog/views.py
+++ Blog_project/Blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.template import loader
 
 # Create your views here.
 
@@ -14,3 +16,14 @@ def delete(request):
 
 def edit(request):
     return HttpResponse("This is the blog edit page")
+
+def detail(request, id):
+    return HttpResponse("<h2>Details for album id:" + str(id)+ "<h2>")
+
+def test(request):
+    all_posts = Post.objects.all()
+    template = loader.get_template("Blog/index.html")
+    context = {
+    'all_posts': all_posts
+    }
+    return HttpResponse(template.render(context, request))
